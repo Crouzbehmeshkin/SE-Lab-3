1bdca4544792f22042577dae48cb7cf7e50bb34a
Rouzbeh Meshkinnejad <rouzyd@gmail.com>
2021-04-09 20:42:25 +0430

    Fixed double and int problems

--- 2-Bridge/src/main/java/PowerCalculator.java
+++ 2-Bridge/src/main/java/PowerCalculator.java
@@ -5,7 +5,7 @@ public class PowerCalculator {
         this.power_implementor = power_implementor;
     }
 
-    public double calculate(double base, int power) {
+    public double calculate(int base, int power) {
         return this.power_implementor.operation(base, power);
     }
 }
--- 2-Bridge/src/test/java/PowerTest.java
+++ 2-Bridge/src/test/java/PowerTest.java
@@ -7,40 +7,40 @@ public class PowerTest {
     @Test
     public void test1() {
         PowerCalculator power_calculator = new PowerCalculator(new PowerImplementor1(new MultImplementor1()));
-        assertEquals(32, power_calculator.calculate(2, 5), 1e-4);
-        assertEquals(81, power_calculator.calculate(3, 4), 1e-4);
-        assertEquals(1, power_calculator.calculate(100, 0), 1e-4);
-        assertEquals(24.5, power_calculator.calculate(24.5, 1), 1e-4);
-        assertEquals(24.5 * 24.5, power_calculator.calculate(24.5, 2), 1e-4);
+        assertEquals(32, power_calculator.calculate(2, 5));
+        assertEquals(81, power_calculator.calculate(3, 4));
+        assertEquals(1, power_calculator.calculate(100, 0));
+        assertEquals(24, power_calculator.calculate(24, 1));
+        assertEquals(24 * 24, power_calculator.calculate(24, 2));
     }
 
     @Test
     public void test2() {
         PowerCalculator power_calculator = new PowerCalculator(new PowerImplementor1(new MultImplementor2()));
-        assertEquals(32, power_calculator.calculate(2, 5), 1e-4);
-        assertEquals(81, power_calculator.calculate(3, 4), 1e-4);
-        assertEquals(1, power_calculator.calculate(100, 0), 1e-4);
-        assertEquals(24.5, power_calculator.calculate(24.5, 1), 1e-4);
-        assertEquals(24.5 * 24.5, power_calculator.calculate(24.5, 2), 1e-4);
+        assertEquals(32, power_calculator.calculate(2, 5));
+        assertEquals(81, power_calculator.calculate(3, 4));
+        assertEquals(1, power_calculator.calculate(100, 0));
+        assertEquals(24, power_calculator.calculate(24, 1));
+        assertEquals(24 * 24, power_calculator.calculate(24, 2));
     }
 
     @Test
     public void test3() {
         PowerCalculator power_calculator = new PowerCalculator(new PowerImplementor2(new MultImplementor1()));
-        assertEquals(32, power_calculator.calculate(2, 5), 1e-4);
-        assertEquals(81, power_calculator.calculate(3, 4), 1e-4);
-        assertEquals(1, power_calculator.calculate(100, 0), 1e-4);
-        assertEquals(24.5, power_calculator.calculate(24.5, 1), 1e-4);
-        assertEquals(24.5 * 24.5, power_calculator.calculate(24.5, 2), 1e-4);
+        assertEquals(32, power_calculator.calculate(2, 5));
+        assertEquals(81, power_calculator.calculate(3, 4));
+        assertEquals(1, power_calculator.calculate(100, 0));
+        assertEquals(24, power_calculator.calculate(24, 1));
+        assertEquals(24 * 24, power_calculator.calculate(24, 2));
     }
 
     @Test
     public void test4() {
         PowerCalculator power_calculator = new PowerCalculator(new PowerImplementor2(new MultImplementor2()));
-        assertEquals(32, power_calculator.calculate(2, 5), 1e-4);
-        assertEquals(81, power_calculator.calculate(3, 4), 1e-4);
-        assertEquals(1, power_calculator.calculate(100, 0), 1e-4);
-        assertEquals(24.5, power_calculator.calculate(24.5, 1), 1e-4);
-        assertEquals(24.5 * 24.5, power_calculator.calculate(24.5, 2), 1e-4);
+        assertEquals(32, power_calculator.calculate(2, 5));
+        assertEquals(81, power_calculator.calculate(3, 4));
+        assertEquals(1, power_calculator.calculate(100, 0));
+        assertEquals(24, power_calculator.calculate(24, 1));
+        assertEquals(24 * 24, power_calculator.calculate(24, 2));
     }
 }
